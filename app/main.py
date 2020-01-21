7d47b9add0f832c65aeead37997d3b40494d6725
Jitka Novotná <jitka@ucw.cz>
2020-01-21 21:00:05 +0100

    pocitam uzivatele

--- app/main.py
+++ app/main.py
@@ -9,10 +9,18 @@ from flask_api import FlaskAPI, status, exceptions
 
 app = FlaskAPI(__name__)
 
+
+def countMembersHtml(html):
+    pageUsers = html.count('./memberlist.php?mode=viewprofile&amp;')
+    unregistred = html.count('Uživatel fóra – není člen Pirátů')
+    registred = html.count('Příznivec Pirátů –')
+    return pageUsers - unregistred - registred
+
 def countMembers(total):
+    #TODO prvni a posledni
     for i in range(100,total,100):
         r = requests.get('https://forum.pirati.cz/memberlist.php?start='+str(i))
-        print(r.text)
+        print(countMembersHtml(r.text))
 
 @app.route('/members/')
 def example():
