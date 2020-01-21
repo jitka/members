718e4ef57987b4aaea47511b3b760e2dd6ca65c7
Jitka Novotná <jitka@ucw.cz>
2020-01-21 20:42:37 +0100

    na rucni vraci pocet

--- app/main.py
+++ app/main.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
-from members import create_app
+#https://www.flaskapi.org/
 
-app = create_app()
+from members import create_app #TODO promazat import
+from flask import request, url_for
+from flask_api import FlaskAPI, status, exceptions
+
+app = FlaskAPI(__name__)
+
+@app.route('/members/')
+def example():
+    return {'members': 11488}
+
+if __name__ == "__main__":
+    app.run(debug=True)
